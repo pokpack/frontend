93e02e56cd3116cec558d7a6f309a68761396046
nattanon <nattanon@hospital-os.com>
2020-12-08 21:29:35 +0700

    add config

--- nurse_web/application/controllers/Api.php
+++ nurse_web/application/controllers/Api.php
@@ -1,13 +1,15 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Api extends CI_Controller {
+class Api extends CI_Controller
+{
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
         $this->load->library('pagination');
-	$this->load->model('Api_model');
+        $this->load->model('Api_model');
         $this->load->model('Main_model');
     }
 
@@ -26,13 +28,14 @@ class Api extends CI_Controller {
      * map to /index.php/welcome/<method_name>
      * @see https://codeigniter.com/user_guide/general/urls.html
      */
-    public function test() {
+    public function test()
+    {
 
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
             CURLOPT_PORT => "3001",
-            CURLOPT_URL => "http://localhost:3001/EMRs/",
+            CURLOPT_URL => getenv('EMR_API_URL') . "/EMRs/",
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
             CURLOPT_MAXREDIRS => 10,
@@ -54,9 +57,10 @@ class Api extends CI_Controller {
         echo $response;
     }
 
-    public function post_admit() {
-//        echo json_encode($_POST);
-//        exit();
+    public function post_admit()
+    {
+        //        echo json_encode($_POST);
+        //        exit();
         $_select = array('*');
         $arr_where = array('id' => 1);
         $config = $this->Main_model->rowdata(TBL_CONFIG, $arr_where, $_select);
@@ -65,8 +69,8 @@ class Api extends CI_Controller {
         $emr = $config->s_value;
         $curl = curl_init();
 
-        $url = "http://localhost:3001/api/" . $hn . "/admit/" . $emr;
-//        $_POST[datetime] = date('Y-m-d H:i:s');
+        $url = getenv('EMR_API_URL') . "/api/" . $hn . "/admit/" . $emr;
+        //        $_POST[datetime] = date('Y-m-d H:i:s');
         curl_setopt_array($curl, array(
             CURLOPT_PORT => "3001",
             CURLOPT_URL => $url,
@@ -84,34 +88,32 @@ class Api extends CI_Controller {
                 "postman-token: 75c76a80-be0e-b7a7-4ce3-5dcce07faefc"
             ),
         ));
-//
+        //
         $response = curl_exec($curl);
         $err = curl_error($curl);
-//
+        //
         curl_close($curl);
 
         $data[s_value] = intval($emr) + 1;
         $data[result] = $this->db->update(TBL_CONFIG, $data, array('id' => 1));
-        
+
         $return[update] = $data;
         $return[url] = $url;
         $return[res] = $response;
-        
-//        $this->session->set_userdata(array('savedata' => 1));
+
+        //        $this->session->set_userdata(array('savedata' => 1));
         setcookie("savedata", 1, time() + 3600, '/');
         echo json_encode($return);
-//        echo json_encode($_POST);
+        //        echo json_encode($_POST);
     }
 
-    public function get_admit() {
-        
+    public function get_admit()
+    {
     }
-    
-    public function post_treat() {
+
+    public function post_treat()
+    {
         $data = $this->Api_model->post_treat();
         echo $data;
     }
-    
-   
-
 }
